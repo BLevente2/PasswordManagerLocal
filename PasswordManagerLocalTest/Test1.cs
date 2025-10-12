a3b2de3e810e6f1474b4bafc4fe74f5cc4dff58b
Levente Baba <53221679+BLevente2@users.noreply.github.com>
2025-10-12 11:25:27 +0200

    Add project files.

---
+++ PasswordManagerLocalTest/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace PasswordManagerLocalTest
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
